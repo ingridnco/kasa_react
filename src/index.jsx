0d7a622c18e66eadf73c7d2243e92aaa34c10240
Ingrid Seurin <ingridseurin@gmail.com>
2024-09-02 16:01:50 +0200

    déplacement Page Error

--- src/index.jsx
+++ src/index.jsx
@@ -7,7 +7,7 @@ import Home from "./pages/Home"
 import About from "./pages/About"
 import Header from "./components/Header"
 import Footer from "./components/Footer"
-import Error from "./components/Error"
+import Error from "./pages/Error"
 import Logement from "./pages/Logement"
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
