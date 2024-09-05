edf1824014ad96a65ff7912f55aa74a572c5a240
Ingrid Seurin <ingridseurin@gmail.com>
2024-09-05 08:40:04 +0200

    correction alt logo header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import logo_kasa from "../../assets/logo_kasa.webp"
 const Header = () => {
   return (
     <nav className="Header">
-      <img src={logo_kasa} className="Header__logo" alt="#" />
+      <img src={logo_kasa} className="Header__logo" alt="logo Kasa" />
       <div className="Header__links">
         <Link to="/">Accueil</Link>
         <Link to="/about">A Propos</Link>
